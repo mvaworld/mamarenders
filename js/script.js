d737e3472231ad6b213078a70d376772e6be1f3a
mvaworld <121222210+mvaworld@users.noreply.github.com>
2022-12-22 11:24:24 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+jQuery(function ($) {
+  $('.musicstart').on('click', function () {
+    $('.music')[0].play()
+  });
+  $('.music').hide();
+});
+
+console.log('I hope you brushed your teeth before bed');
